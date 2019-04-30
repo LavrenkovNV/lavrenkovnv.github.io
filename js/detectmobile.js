655f60bb535f9bc580953b3ecef5514d642f4a4b
Лавренков Никита Владимирович <LavrenkovNV@users.noreply.github.com>
2019-04-30 14:37:52 +0300

    ывф

--- js/detectmobile.js
+++
@@ -1,6 +0,0 @@
-        function detectmob() {
-            if (typeof window.orientation !== 'undefined') {
-                return true;
-            }
-            return false;
-        }
--- js/scroll.js
+++
@@ -1,6 +0,0 @@
-function autoScrolling() {
-                        //var elem = document.getElementById("hideme");
-                        //elem.remove();
-                     // window.scrollTo(0,document.body.scrollHeight);
-                     window.scrollTo(0,9999999999999999999);}
-                     setInterval(autoScrolling, 1000); 
